44b6ceaa34dbe775d53e8abd622fed7ece2dcf83
miller45 <gmse45forse@googlemail.com>
2024-12-26 20:47:40 +0100

    latest version

--- CurtainControl.ino
+++ CurtainControl.ino
@@ -1,5 +1,5 @@
-//#define TESTMODE
 //#define LOGINPUT
+//#define SWEEP
 // FabScan - http://hci.rwth-aachen.de/fabscan
 //
 // R. Bohne 30.12.2013
@@ -17,12 +17,22 @@
 #define ENABLE_PIN_0  4
 #define STEP_PIN_0    3
 #define DIR_PIN_0     2
-#define CWPIN 9
-#define CCWPIN 10
+#define CWPIN 10
+#define CCWPIN 11
 
-#define STDSPEED 100
+#define FAN_PIN 9
+
+#define STDSPEED 20f
+
+#define MODE_IDLE 0
+#define MODE_STANDBY 1
+#define SPEED1 2
+#define SPEED2 3
+#define SPEED3 4
 
 int stepMul = 1;
+
+int mode = MODE_IDLE;
 
 
 bool lastDir = LOW;
@@ -31,85 +41,108 @@ AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN_0, DIR_PIN_0);
 
 void setup()
 {
-  digitalWrite(ENABLE_PIN_0, INPUT);  //STDBY
+  digitalWrite(ENABLE_PIN_0, HIGH);
   Serial.begin(9600);
+#ifdef SWEEP
+  Serial.println("SWEEP");
+#endif
   pinMode(CWPIN, INPUT_PULLUP);
   pinMode(CCWPIN, INPUT_PULLUP);
 
   pinMode(LIGHT_PIN, OUTPUT);
-  
+  pinMode(FAN_PIN, OUTPUT);
+
   pinMode(DIR_PIN_0, OUTPUT);
   pinMode(STEP_PIN_0, OUTPUT);
 
-  enableStepper();
 
   stepper.setEnablePin(ENABLE_PIN_0);
 
   stepper.setMaxSpeed(500 * stepMul);
-  stepper.setAcceleration(1000.0 * stepMul); 
+  stepper.setAcceleration(1000.0 * stepMul);
 }
 
 int lldir = 0;
 
-#ifdef TESTMODE
 
-void loop(){
-   //blink all leds, lights ans the laser 10 times
-  for (int i = 0; i < 10000; i++)
-  {
-    digitalWrite(ENABLE_PIN_0, LOW);  
-    digitalWrite(STEP_PIN_0, HIGH);  
-    digitalWrite(DIR_PIN_0, LOW);     
-    digitalWrite(LIGHT_PIN, HIGH);     
-    delay(1000);
-    digitalWrite(ENABLE_PIN_0, HIGH);  
-    digitalWrite(STEP_PIN_0, LOW);  
-    digitalWrite(DIR_PIN_0, HIGH);     
-    digitalWrite(LIGHT_PIN, LOW);     
-    delay(1000);
-  }
+void loop() {
+
+#ifdef LOGINPUT
+  logLoop();
+#elifdef SWEEP
+  sweepLoop();
+#else
+  productiveLoop();
+#endif
+
 
 }
 
-#else
+void sweepLoop() {
+  stepper.move(199 * stepMul);
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+  }
+  delay(500);
+  stepper.move(-199 * stepMul);
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+  }
+  delay(500);
+}
 
-void loop() {
+void logLoop() {
   int dir = 1 - digitalRead(CWPIN) - (1 - digitalRead(CCWPIN));
 
-#ifdef LOGINPUT  
   Serial.println(dir);
   delay(500);
-#else
-  //stepper.runSpeed();
+}
+
+long startmillis = 0;
+long stopmillis = 0;
+
+void productiveLoop() {
+  int dir = 1 - digitalRead(CWPIN) - (1 - digitalRead(CCWPIN));
+
+  //condition changed block
   if (dir != lldir) {
     lldir = dir;
+    startmillis = millis();
     if (dir != 0) {
       enableStepper();
       stepper.setSpeed(dir * STDSPEED * stepMul);
-    } else {      
+     // mode = SPEED1;
+    } else {
       stepper.setSpeed(0);
     }
   }
-  if (dir != 0) {    
+
+  //run always block
+
+  if (dir != 0) {
     stepper.runSpeed();
-  } else {
-    disableStepper();
+    //after 2 sec go 2x faster
+    long milldiff = millis() - startmillis;
+
+    if (milldiff > 2000 && milldiff < 2010) {
+      stepper.setSpeed(dir * STDSPEED * 2 * stepMul);
+    }
+
+  } else { // if no dir transition to standby and then idle
+    standbyStepper();
   }
-#endif
-
 }
-
-#endif
 
 
 int stepperState = -2;
 
 void enableStepper() {
-  if (stepperState != 2) { 
+  if (stepperState != 2) {
     stepperState = 2;
     pinMode(ENABLE_PIN_0, OUTPUT);
     digitalWrite(ENABLE_PIN_0, LOW);
-    digitalWrite(LIGHT_PIN,HIGH);
+    digitalWrite(LIGHT_PIN, HIGH);
+    digitalWrite(FAN_PIN, HIGH);
   }
 }
 void disableStepper() {
@@ -117,7 +150,8 @@ void disableStepper() {
     stepperState = 0;
     pinMode(ENABLE_PIN_0, OUTPUT);
     digitalWrite(ENABLE_PIN_0, HIGH);
-    digitalWrite(LIGHT_PIN,LOW);
+    digitalWrite(LIGHT_PIN, LOW);
+    digitalWrite(FAN_PIN, LOW);
   }
 }
 
@@ -126,7 +160,8 @@ void standbyStepper() {
     stepperState = 1;
     digitalWrite(ENABLE_PIN_0, LOW);
     pinMode(ENABLE_PIN_0, INPUT);
-    digitalWrite(LIGHT_PIN,HIGH);
+    digitalWrite(LIGHT_PIN, HIGH);
+    digitalWrite(FAN_PIN, HIGH);
   }
 }
 
